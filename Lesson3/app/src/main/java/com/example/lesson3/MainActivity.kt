a96d269958367d13e61b424379c43207f637f034
Abraham Dereje Abera <abrahamderej@gmail.com>
2022-02-20 19:34:29 -0600

    Lesson3

---
+++ Lesson3/app/src/main/java/com/example/lesson3/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lesson3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var foods = ArrayList<String>(arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnDecideClick(view: View){
+        txt_food_name.text = getRandomFood(foods);
+    }
+
+    private fun getRandomFood(fastFoods: ArrayList<String>): String {
+        var randomFoodIndex = Random.nextInt(0, fastFoods.size);
+        return fastFoods[randomFoodIndex];
+    }
+
+    fun btnAddFoodClick(view: View){
+        var fastFoodName = text_add_new_food.text.toString();
+        foods.add(fastFoodName);
+        text_add_new_food.setText("");
+    }
+}
